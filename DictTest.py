fb28b4afd1c190a18f3049fcf09294a3324eee6e
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-25 14:47:19 +0900

    グラフ表示を追加

--- DictTest.py
+++ DictTest.py
@@ -8,8 +8,15 @@ def hello(name=None):
     my_dic = {}
     my_dic['name'] = name
     my_dic['univ'] = "TOKYO Univ."
-    return render_template('dict.html', message=my_dic)
+    return render_template('chart.html', message=my_dic)
 
-#モジュールを実行する場合の処理
+@app.route('/showgraph/')
+def showgraph():
+    d_list = ['3月25日', '3月26日','3月27日','3月28日','3月29日','3月30日']
+    high_temp_list = [16,18,21,21,11,14]
+    low_temp_list = [7,9,17,7,7,9]
+    return render_template('chart.html', message=d_list, temp=high_temp_list, lowtemp=low_temp_list)
+
+#日本語
 if __name__ == "__main__":
     app.run(debug=True)
