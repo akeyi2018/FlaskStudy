3bab50f02aa92f0413b95beed8f29d6e0a2858f9
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-03-31 18:17:18 +0900

    Create weekweather.py

---
+++ weekweather.py
@@ -0,0 +1,79 @@
+import requests
+import re
+import urllib3
+from bs4 import BeautifulSoup
+
+class Weather:
+
+    def __init__(self):
+
+        urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+        res = requests.get("https://www.jma.go.jp/jp/week/319.html", verify=False)
+        soup = BeautifulSoup(res.text,"html.parser")
+        self.title = soup.find("caption").text
+        self.days = soup.find("table", {"id": "infotablefont"}).find_all("tr")[0].find_all("th")
+        self.weatherinfo = soup.find("table", {"id": "infotablefont"}).find_all("tr")[1].find_all("td")
+        self.max_temp = soup.find("table", {"id": "infotablefont"}).find_all("tr")[4].find_all("td")
+        self.min_temp = soup.find("table", {"id": "infotablefont"}).find_all("tr")[5].find_all("td")
+        self.hiduke = '日付'
+
+    def getInfo(self):
+
+        weather = []
+        daylist = []
+        weeklist = []
+        info = self.weatherinfo[0:7]
+
+        for daytemp in self.days:
+            daytemp = daytemp.text
+
+            if self.hiduke not in daytemp:
+                daylist.append(daytemp[:-1])
+                weeklist.append(daytemp[-1])
+
+        #for infotemp in self.weatherinfo:
+        #    info.append(infotemp.text)
+
+        maxtemplist = []
+        for maxtemp in self.max_temp:
+            maxtemp = maxtemp.text
+
+            if '最高' not in maxtemp:
+                maxtemp = maxtemp.replace('\n', '').replace('\t', '').replace('(', '{').replace(')', '}')
+                maxtemp = re.sub('{.*?}', '', maxtemp)
+
+                if '／' in maxtemp:
+                    maxtemp = None
+                else:
+                    maxtemp = int(maxtemp)
+
+                maxtemplist.append(maxtemp)
+
+        mintemplist = []
+        for mintemp in self.min_temp:
+            mintemp = mintemp.text
+
+            if '最低' not in mintemp:
+                mintemp = mintemp.replace('\n', '').replace('\t', '').replace('(', '{').replace(')', '}')
+                mintemp = re.sub('{.*?}', '', mintemp)
+
+                if '／' in mintemp:
+                    mintemp = None
+                else:
+                    mintemp = int(mintemp)
+
+                mintemplist.append(mintemp)
+
+        weather.append(self.title)
+        #weather.append(info)
+        weather.append(daylist)
+        weather.append(maxtemplist)
+        weather.append(mintemplist)
+
+        return weather
+
+if __name__ == "__main__":
+
+    wt = Weather()
+    print(wt.getInfo()[1])
