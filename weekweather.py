678ece05490948ef6993f8f1da63cd8d47785cd3
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-04-01 17:42:11 +0900

    Update weekweather.py

--- weekweather.py
+++ weekweather.py
@@ -16,35 +16,22 @@ class Weather:
         self.weatherinfo = soup.find("table", {"id": "infotablefont"}).find_all("tr")[1].find_all("td")
         self.max_temp = soup.find("table", {"id": "infotablefont"}).find_all("tr")[4].find_all("td")
         self.min_temp = soup.find("table", {"id": "infotablefont"}).find_all("tr")[5].find_all("td")
-        self.hiduke = '日付'
+        self.hiduke = ['日付','最高','最低','／']
 
     def getInfo(self):
 
         weather = []
-        daylist = []
         weeklist = []
-        info = []
-
-        for daytemp in self.days:
-            daytemp = daytemp.text
-
-            if self.hiduke not in daytemp:
-                daylist.append(daytemp[:-1] + '(' + daytemp[-1] + ')')
-
-        targetInfo = self.weatherinfo[0:7]
-        for infotemp in targetInfo:
-            infotemp = infotemp.text
-            info.append(infotemp.replace('\n',''))
 
         maxtemplist = []
         for maxtemp in self.max_temp:
             maxtemp = maxtemp.text
 
-            if '最高' not in maxtemp:
+            if self.hiduke[1] not in maxtemp:
                 maxtemp = maxtemp.replace('\n', '').replace('\t', '').replace('(', '{').replace(')', '}')
                 maxtemp = re.sub('{.*?}', '', maxtemp)
 
-                if '／' in maxtemp:
+                if self.hiduke[3] in maxtemp:
                     maxtemp = None
                 else:
                     maxtemp = int(maxtemp)
@@ -55,11 +42,11 @@ class Weather:
         for mintemp in self.min_temp:
             mintemp = mintemp.text
 
-            if '最低' not in mintemp:
+            if self.hiduke[2] not in mintemp:
                 mintemp = mintemp.replace('\n', '').replace('\t', '').replace('(', '{').replace(')', '}')
                 mintemp = re.sub('{.*?}', '', mintemp)
 
-                if '／' in mintemp:
+                if self.hiduke[3] in mintemp:
                     mintemp = None
                 else:
                     mintemp = int(mintemp)
@@ -67,8 +54,8 @@ class Weather:
                 mintemplist.append(mintemp)
 
         weather.append(self.title)
-        weather.append(info)
-        weather.append(daylist)
+        weather.append([infotemp.text.replace('\n','') for infotemp in self.weatherinfo[0:7]])
+        weather.append([dat.text[:-1] + '(' + dat.text[-1] + ')' for dat in self.days if self.hiduke[0] not in dat.text])
         weather.append(maxtemplist)
         weather.append(mintemplist)
 
@@ -77,4 +64,4 @@ class Weather:
 if __name__ == "__main__":
 
     wt = Weather()
-    print(wt.getInfo()[1])
+    print(wt.getInfo()[2])
