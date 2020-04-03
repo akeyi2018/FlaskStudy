e13d9f3cd63784f2d216e57b247b03ee5e5e4b86
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-04-03 14:01:14 +0900

    Update ShowWeekWeaher.py

--- ShowWeekWeaher.py
+++ ShowWeekWeaher.py
@@ -22,14 +22,10 @@ def showgraph():
         wt = Weather(str(request.form["pref"])).getInfo()
         selectval = str(request.form["pref"])
 
-    title = str(wt[0])
-    d_list = []
-    for winfo, days in zip(wt[1],wt[2]):
-        d_list.append(days + '(' + winfo +')')
-    high_temp_list = wt[3]
-    low_temp_list = wt[4]
-
-    return render_template('chart.html', selectedval = selectval, preflink=getpref(), mes=title, message=d_list, temp=high_temp_list, lowtemp=low_temp_list)
+    return render_template('chart.html', selectedval = selectval,
+                           preflink=getpref(),
+                           mes=str(wt[0]), message=wt[2], weatherinfo=wt[1],
+                           temp=wt[3], lowtemp=wt[4])
 
 if __name__ == "__main__":
     app.run(debug=True)
