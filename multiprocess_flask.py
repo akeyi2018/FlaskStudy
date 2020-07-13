f835adda7e6fbe9e691a0f7bfa4da2e6ef5c1135
akeyi <34048522+akeyi2018@users.noreply.github.com>
2020-07-14 02:26:28 +0900

    Create multiprocess_flask.py

---
+++ multiprocess_flask.py
@@ -0,0 +1,28 @@
+from multiprocessing import Process
+from flask import Flask
+from time import sleep
+
+app = Flask(__name__)
+
+def testFun():
+    print('Starting')
+    for cn in range(20):
+        print(str(cn) + ' Seconds Later')
+        sleep(1)
+
+@app.route('/kill')
+def killproc():
+
+    backProc.terminate()
+    return "Killed Porcess "
+
+@app.route('/')
+def root():
+    global backProc
+    backProc = Process(target=testFun, args=())
+    backProc.start()
+    return "Started a background process"
+
+if __name__ == '__main__':
+
+    app.run(debug=True, host='0.0.0.0')
