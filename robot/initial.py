e6ccf39249acbf3751688e00c268a4a9485a7805
akeyi2018 <akeyi2016@gmail.com>
2021-04-08 08:23:28 +0900

    test

--- robot/initial.py
+++ robot/initial.py
@@ -1,17 +1,14 @@
-# from gpiozero import LED
+from gpiozero import LED
 from time import sleep
 from controller import robot_controller
 import os
 
 control = robot_controller(os.path.dirname(os.path.realpath(__file__)))
-config = control.get_config()
 
-print(control.get_config()['Robot'])
+for pin in control.get_config()['Robot']:
+    re = LED(pin)
 
-# for pin in config['Robot']:
-#     re = LED(pin)
+led = LED(5)
 
-# led = LED(5)
-
-# led.blink()
-# sleep(1)
+led.blink()
+sleep(1)
