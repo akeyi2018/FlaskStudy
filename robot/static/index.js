b437daf7761e2159169b010d8b71ae3f77d114b7
akeyi2018 <akeyi2016@gmail.com>
2021-04-08 20:05:54 +0900

    temp

--- robot/static/index.js
+++ robot/static/index.js
@@ -5,40 +5,46 @@ window.onload = () => {
         output_info('move to front', 1);
     };
 
-    $front_elem.onmouseup = () => {
-        $front_elem.innerHTML = '前進';
-        output_info('stop to move front!', 0);
-    };
+    // $front_elem.onmouseup = () => {
+    //     $front_elem.innerHTML = '前進';
+    //     output_info('stop to move front!', 0);
+    // };
     var $back_elem = document.getElementById("back");
     $back_elem.onmousedown = () => { 
         $back_elem.innerHTML = 'MOVE';
         output_info('move to back', 2);
     };
 
-    $back_elem.onmouseup = () => {
-        $back_elem.innerHTML = '後退';
-        output_info('stop to move back', 0);
-    };
+    // $back_elem.onmouseup = () => {
+    //     $back_elem.innerHTML = '後退';
+    //     output_info('stop to move back', 0);
+    // };
     var $left_elem = document.getElementById("left");
     $left_elem.onmousedown = () => { 
         $left_elem.innerHTML = 'MOVE';
         output_info('move to left', 3);
     };
 
-    $left_elem.onmouseup = () => {
-        $left_elem.innerHTML = '後退';
-        output_info('stop to move back', 0);
-    };
+    // $left_elem.onmouseup = () => {
+    //     $left_elem.innerHTML = '後退';
+    //     output_info('stop to move back', 0);
+    // };
     var $right_elem = document.getElementById("right");
     $right_elem.onmousedown = () => { 
         $right_elem.innerHTML = 'MOVE';
         output_info('move to right', 4);
     };
 
-    $right_elem.onmouseup = () => {
-        $right_elem.innerHTML = '後退';
-        output_info('stop to move back', 0);
+    var $right_elem = document.getElementById("stop");
+    $right_elem.onmousedown = () => { 
+        $right_elem.innerHTML = 'MOVE';
+        output_info('move to right', 0);
     };
+
+    // $right_elem.onmouseup = () => {
+    //     $right_elem.innerHTML = '後退';
+    //     output_info('stop to move back', 0);
+    // };
 }
  
 function output_info($text_info, direction) {
