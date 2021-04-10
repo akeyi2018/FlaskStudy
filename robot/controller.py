395d3110d69a197a7e149a2b9b4455fda4605c8e
akeyi2018 <akeyi2016@gmail.com>
2021-04-10 22:08:26 +0900

    test

--- robot/controller.py
+++ robot/controller.py
@@ -55,8 +55,8 @@ class SensingDistance:
     #     print('set status one')
 
     def run(self):
-        self.sensor.when_deactivated = lambda : print('zero') 
-        self.sensor.when_activated = lambda : print('one')
+        self.sensor.when_deactivated = print(lambda : 0)
+        self.sensor.when_activated = print(lambda : 1)
         pause() 
 
 
