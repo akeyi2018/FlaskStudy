10d0a844ae43bf750cdb35fb724b0cdf5a373f41
akeyi2018 <akeyi2016@gmail.com>
2021-04-10 20:58:51 +0900

    test

--- robot/distans.py
+++ robot/distans.py
@@ -3,13 +3,16 @@ from signal import pause
 
 sensor = DistanceSensor(27,17)
 
-# led = LED(5)
+led = LED(5)
 
-while True:
-    print('Distance to nearest object is', sensor.distance, 'm')
-    sleep(1)
+# while True:
+    # print('Distance to nearest object is', sensor.distance, 'm')
+    # sleep(1)
 
-# sensor.when_activated
+def test():
+    print('Active')
+
+sensor.when_activated = test() 
 # sensor.when_in_range = led.on
 # sensor.when_out_of_range = led.off
 
