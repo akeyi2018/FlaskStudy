d5429ea700601938d0704fd8a2dcf8bac90f0d29
akeyi2018 <akeyi2016@gmail.com>
2021-04-10 10:35:32 +0900

    add dis

--- robot/distans.py
+++ robot/distans.py
@@ -1,10 +1,16 @@
 from gpiozero import DistanceSensor, LED
 from signal import pause
 
-sensor = DistanceSensor(27,17, max_distance=1, threshold_distance=0.2)
-led = LED(5)
+sensor = DistanceSensor(27,17)
 
-sensor.when_in_range = led.on
-sensor.when_out_of_range = led.off
+# led = LED(5)
 
-pause()
+while True:
+    print('Distance to nearest object is', sensor.distance, 'm')
+    sleep(1)
+
+# sensor.when_activated
+# sensor.when_in_range = led.on
+# sensor.when_out_of_range = led.off
+
+# pause()
