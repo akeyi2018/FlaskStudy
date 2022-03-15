e497f27ab16c72e098345ebe37c547311ccc5abb
akeyi2018 <akeyi2016@gmail.com>
2022-03-15 21:31:56 +0900

    Update

--- weather/ShowWeekWeaher.py
+++ weather/ShowWeekWeaher.py
@@ -14,13 +14,19 @@ def showgraph():
     selectval = "千葉県"
     wt = Weather(selectval).addInfo()
     if request.method == 'POST':
-        print(str(request.form["pref"]))
+        # print(str(request.form["pref"]))
         wt = Weather(str(request.form["pref"])).addInfo()
         selectval = str(request.form["pref"])
-        # print(wt[1])
     # _が使えない
-    print(wt)
-    return render_template('chart.html', da1 = wt)
+    return render_template('chart.html', da1 = wt, preflink = getpref(), selectedval = selectval)
+
+@app.route('/u', methods = ['GET','POST'])
+def set_date():
+    if request.method == 'POST':
+        fr = str(request.form["from"])
+        to = str(request.form["to"])
+        res = f'from:{fr},to:{to}'
+        return res
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
--- weather/static/js/main.js
+++ weather/static/js/main.js
@@ -1,21 +1,20 @@
-// var data = JSON.parse('[1,2,3,4,5]');
-console.info(da);
+// console.info(da);
 const ctx = document.getElementById('myChart');
 
 const chart = new Chart(ctx, {
     type: 'bar',
     data: {
-        labels: da[0],
+        labels: da[1],
         datasets: [{
             label: 'data_01',
-            data: da[1],
+            data: da[2],
             borderColor: "rgba(255,255,0,1)",
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)'
             ],
         },{
             label:'data_02',
-            data: da[2],
+            data: da[3],
             borderColor: "rgba(0,0,255,1)",
             backgroundColor: [
                 'rgba(25, 0, 132, 0.2)'
