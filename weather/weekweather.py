5ed43c1dfc911b93147caf433ed3484ac389d26c
akeyi2018 <akeyi2016@gmail.com>
2022-03-15 21:31:56 +0900

    Update

--- weather/weekweather.py
+++ weather/weekweather.py
@@ -20,14 +20,14 @@ class Weather(prefecture):
 
         self.pre_list = self.getPrefecture()
         self.num = self.pre_list[pref]
-        self.title = 'グラフサンプル'
+        self.title = pref
         self.labels = [2017,2018,2019,2020,2021]
         self.data_01 = [30,25,28,21,20,12]
         self.data_02 = [10,15,20,10,12,18]
         self.data_03 = [3, 4, 2, 4, 2]
 
     def addInfo(self):
-        weather = [self.labels, self.data_01, self.data_02]
+        weather = [self.title, self.labels, self.data_01, self.data_02]
         return weather
     
     def add2dInfo(self):
