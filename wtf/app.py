6d860406213524fdf97f309f0a2bffb64e1e6cf9
akeyi2018 <akeyi2016@gmail.com>
2022-03-16 22:24:20 +0900

    show default date

--- wtf/app.py
+++ wtf/app.py
@@ -1,26 +1,30 @@
+from collections import defaultdict
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from wtforms.fields import DateField
+from datetime import date, datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'test secrect key'
 
 class NameForm(FlaskForm):
-    name = StringField('name:', validators=[DataRequired()])
-    inputdate = DateField('日時:', format='%Y-%m-%d')
-    submit = SubmitField('Go')
+    start_date = DateField('start date:', format='%Y-%m-%d',default=date(2000,4,1))
+    end_date = DateField('end date:', format='%Y-%m-%d', default= datetime.today())
+    submit = SubmitField('submit')
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/s', methods=['GET', 'POST'])
 def index():
-    name = None
+    start_d = None
+    end_d = None
     form = NameForm()
-    if form.validate_on_submit():
-        name = form.inputdate.data
     if form.is_submitted():
-        input_date = form.inputdate.data
-    return render_template('index.html', form=form, name=input_date)
+        start_d = form.start_date.data
+        end_d = form.end_date.data
+        print(type(start_d))
+
+    return render_template('index.html', form=form, start_date = start_d, end_date=end_d)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
