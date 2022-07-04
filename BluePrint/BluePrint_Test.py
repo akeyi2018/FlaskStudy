4a40e1720542f460dcbfa42bc5d695f984cd9659
akeyi2018 <akeyi2016@gmail.com>
2022-07-05 06:00:13 +0900

    BluePrintリダイレクト機能追加

--- BluePrint/BluePrint_Test.py
+++ BluePrint/BluePrint_Test.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 from admin.api import Api
 
 app = Flask(__name__)
 
-app.register_blueprint(Api,url_prefix='/admin')
+app.register_blueprint(Api, url_prefix='/admin')
+
+@app.route("/")
+def home():
+    return 'Home'
+
+@app.route('/go')
+def go():
+    # BluePrint先へリダイレクト
+    return redirect(url_for('api.hoge'))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
 
--- BluePrint/admin/api.py
+++ BluePrint/admin/api.py
@@ -1,8 +1,14 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, render_template, url_for
 
 Api = Blueprint('api', __name__)
 
 @Api.route('/login')
 def hoge():
-    return 'Login'
 
+    return render_template('login.html')
+
+
+@Api.route('/goto_main')
+def gotomain():
+    # メイン画面へリダイレクト
+    return redirect(url_for('home'))
