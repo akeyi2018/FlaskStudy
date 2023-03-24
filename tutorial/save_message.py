185cf1e2996459e9f2233135ec67e25c802b5918
akeyi2018 <akeyi2016@gmail.com>
2023-03-24 13:25:26 +0900

    new file

--- tutorial/save_message.py
+++ tutorial/save_message.py
@@ -5,13 +5,13 @@ import os,csv
 import socket
 
 class Messager():
-    def __init__(self, target='KIX-M', author='admin', message='this is test message') -> None:
+    def __init__(self, target=0, author='admin', message='this is test message') -> None:
         if socket.gethostname() == 'akeyi2021':
-            self.message_folder = './message'
+            self.message_folder = '.\message'
         self.messager_file = 'messager.csv'
-        self.messager_path = os.path.join(self.message_folder, self.messager_file)
+        self.messager_path = os.path.join(os.getcwd(), self.message_folder, self.messager_file)
         self.dt_now = datetime.now()
-        self.target = target
+        self.target = int(target)
         self.author = author
         self.message = message
         self.target_list = ['KIX-M','KIX-D','ITM-M','ITM-D']
@@ -19,29 +19,40 @@ class Messager():
 
     # 新規作成
     def create_new(self):
-        li = [self.dt_now, self.target, self.author, self.message]
-        with open(self.messager_path, 'w', newline='') as f:
+        # メッセージの改行マークの置換
+        # m = self.message.replace('\r\n','&#13;')
+        m = self.message
+        out = [self.dt_now, self.target_list[self.target], self.author, m]
+        with open(self.messager_path, 'w', newline='', encoding='cp932') as f:
             w = csv.writer(f)
             w.writerow(self.csv_col)
-            w.writerow(li)
+            w.writerow(out)
 
     # 追記（更新）
     def update_data(self):
-        li = [self.dt_now, self.target, self.author, self.message]
+        out = [self.dt_now, self.target_list[self.target], self.author, self.message.strip()]
         with open(self.messager_path, 'a', newline='') as f:
             w = csv.writer(f)
-            w.writerow(li)
+            w.writerow(out)
+
     # 読み込み
     def read_data(self):
-        d = {}
+        d = []
         df = pd.read_csv(self.messager_path,encoding='cp932')
         for k in self.target_list:
             res = df.query('target==@k')
             if res.empty:
-                d[k] = ''
+                # d[k] = ''
+                d.append('')
             else:
-                d[k] = res['message'].to_list()[-1]
+                li = self.create_out_message(res.tail(1))
+                # d[k] = li
+                d.append(li)
         return d
+    
+    def create_out_message(self, df):
+        li = df.values.tolist()[0][3]
+        return li
 
     def run(self):
         if os.path.exists(self.messager_path):
@@ -50,10 +61,9 @@ class Messager():
             self.create_new()
 
 if __name__ == "__main__":
-    ins = Messager(target='KIX-D',message='確認しました。2')
-    # ins.run()
+    ins = Messager()
     # 登録
-    ins.run()
+    # ins.run()
 
     # 確認
     d = ins.read_data()
