940a60f0caa2019be394beb03039111ce5b894bc
akeyi <34048522+akeyi2018@users.noreply.github.com>
2023-03-25 00:20:47 +0900

    Update linebot_test.py

--- linebot_test.py
+++ linebot_test.py
@@ -2,8 +2,8 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
 def main():
-    token = "4DCMd5YjF21WwGEve2vM0733BamgnlnE5WsDvOUXMLjo++ofQLC+bNktDToGc8m2C908V3p5QAL1aBkgF1bdRivTww06EVeQpFdk3lQvPkLIzZEHLbZBnWVCUkaS1Rvv+1o6UCJzVSM2XVOUrRGOQwdB04t89/1O/w1cDnyilFU="
-    user_id = 'U79b8610c5f077f92f42752872b967983'
+    token = "dummy_token"
+    user_id = 'dummy_user_name'
     lb = LineBotApi(token)
     message = TextSendMessage(text='おはよう💛')
     lb.push_message(user_id, messages=message)
